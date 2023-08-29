9cd59ac5385b6e3326bf3e75568796bd50de76f5
mariagimenezbustos <134734638+mariagimenezbustos@users.noreply.github.com>
2023-08-29 18:07:12 +0200

    most orders routes working

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const ordersRouter = require("./routes/orders");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api/index", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/orders", ordersRouter);
 
 module.exports = app;
-
