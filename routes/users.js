63e94eab92c8bd31f05bd3e410ee6c2535db78e4
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-08-29 15:16:47 +0100

    methods update

--- routes/users.js
+++ routes/users.js
@@ -4,28 +4,6 @@ const express = require('express');
 const router = express.Router();
 const db = require("../model/helper");
 
-
-// /*helper functions */
-// //returns all the users in ascending order based on key
-
-// function selectAll(req, res, table, key) {
-// 	db(`SELECT * FROM ${table} ORDER BY ${key} ASC;`)
-// 		.then(results => {
-// 		res.send(results.data);
-// 	})
-// 	.catch(err => res.status(500).send(err));
-// };
-
-// //returns a users based on the key you are serching for, eg /users/guest , /users/marketing
-
-// const selectUsersByKey = (req, res, key, value) => {
-// 	db(`SELECT * FROM users WHERE ${key} = "${value}";`)
-// 		.then(results => {
-// 		res.send(results.data);
-// 	})
-// 	.catch(err => res.status(500).send(err));
-// }
-
 // CREATE TABLE `users`(
 //   `id` BIGINT UNSIGNED NULL AUTO_INCREMENT UNIQUE KEY,
 //   `firstname` VARCHAR(255) NOT NULL,
@@ -36,27 +14,70 @@ const db = require("../model/helper");
 //   `marketing` TINYINT(1) NOT NULL DEFAULT 0
 // );
 
+// INSERT INTO users (firstname, lastname, email, password, guest, marketing) 
+// 		VALUES ('random', "Random", 'example@domain.com', "efgrafv`xza32534", 0, 1);
+
 //CREATE
 router.post('/', async function(req, res, next) {
 	const {firstname, lastname, email, password, guest, marketing} = req.body;
-	console.log(req.body);
 	try {
 		const response = await db(`INSERT INTO users (firstname, lastname, email, password, guest, marketing) 
 		VALUES ('${firstname}', '${lastname}', '${email}', '${password}', ${guest}, ${marketing});`);
-    console.log(response);
 		selectAll(req, res, "users", "id");
 	} catch (error) {
-	  res.status(500).send({ error: error.message });
+    res.status(500).send({ error: error.message });
 	}
- });
+});
 
 //READ
 
+//GET all users
 router.get('/', function(req, res) {
 	selectAll(req, res, "users", "id");
 });
 
+/* GET order by order id */
+router.get("/:id", async (req, res) => {
+	const id = req.params.id;
+	console.log(id);
+	console.log(req.query);
+	try {
+		// const results = await db(`SELECT * FROM users where id = ${id};`);
+		const results = selectByKey("users", "id", id);
+		res.send(results);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+
+// router.get('/:id', (req, res) => {
+// 	const key = "id";
+// 	const value = req.params.id;
+// 	selectByKey(req, res, "users", key, value);
+//  });
+
+//DELETE user
+
+router.delete('/:id', async function (req, res, next) {
+	const {id} = req.params;
+	try {
+		console.log("begining of the try");
+		const user = await db(`SELECT * FROM users WHERE id = ${id};`);
+		if(!user.data.length) {
+			console.log("begining of the if");
+			res.send({msg: "User not found"});
+		}
+		console.log(user);
+		await db(`DELETE FROM users WHERE id = ${id};`);
+		console.log('we are here');
+		res.send({msg: 'User deleted!'});
+		
+	} catch (err) {
+		console.log('we are in the catch', err)
+		res.status(500).send(err);
+	}
+});
+
 module.exports = router;
 
-// INSERT INTO users (firstname, lastname, email, password, guest, marketing) 
-// 		VALUES ('random', "Random", 'example@domain.com', "efgrafv`xza32534", 0, 1);
