502b673290b7f2493ceb7917c33ad89f46f675de
Andrea <anfrescortes@gmail.com>
2023-08-30 16:13:04 +0200

    jesus

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
 export default function Products() {
+  const [products, SetProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`/api/products`, {
+          method: "GET",
+        });
+
+        const json = await response.json(); // should I transform everything into a small caps ?
+        SetProducts(json);
+      } catch (error) {
+        res.status(500).send(error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h2>Products</h2>
-      <div className="container"></div>
+      <div className="container">
+        <div></div>
+      </div>
     </div>
   );
 }
