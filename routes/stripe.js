8e8f114ba75e625b32c153f8dfce8c43b5f8bad5
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-08-31 14:42:32 +0100

    front-end updated

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+require("dotenv").config();
+const express = require('express');
+const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_KEY);
-
 
 
 
@@ -12,22 +12,21 @@ router.post('/create-checkout-session', async (req, res) => {
   // })
     const session = await stripe.checkout.sessions.create({
       line_items: [
-        { 
-          price_data: { 
-            currency: "inr", 
-            product_data: { 
-              name: 'product name', 
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: 'product name',
             }, 
-            unit_amount: 30*100, 
-          }, 
-          quantity: 3, 
-        }, 
+            unit_amount: 30*100,
+          },
+          quantity: 3,
+        },
       ],
       mode: 'payment',
       success_url: `http://localhost:5001`,
       cancel_url: `http://localhost:5001`,
     });
-  
     res.send({url: session.url});
   });
   
