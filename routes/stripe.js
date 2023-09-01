b02706b7cb066b0b565a299e9a155b1096648477
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-09-01 11:12:12 +0100

    login functionality done

--- routes/stripe.js
+++ routes/stripe.js
@@ -12,19 +12,19 @@ router.post('/create-checkout-session', async (req, res) => {
   // })
     const session = await stripe.checkout.sessions.create({
       line_items: [
-        { 
-          price_data: { 
-            currency: "euro", 
+        {
+          price_data: {
+            currency: "eur",
             product_data: { 
-              name: 'product name', 
-            }, 
-            unit_amount: 2000, 
+              name: 'product name',
+            },
+            unit_amount: 2000, //this is turned to dollars at check-out on Stripe
           }, 
-          quantity: 3, 
-        }, 
+          quantity: 3,
+        },
       ],
       mode: 'payment',
-      success_url: `http://localhost:5001`,
+      success_url: `http://localhost:5001`, //payment object or a payment intent
       cancel_url: `http://localhost:5001`,
     });
     res.send({url: session.url});
