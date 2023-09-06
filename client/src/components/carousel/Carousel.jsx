0be967734458c7cd520c9a630f9b9c958786afe1
Andrea <anfrescortes@gmail.com>
2023-09-06 12:41:04 +0200

    caroussel

--- client/src/components/carousel/Carousel.jsx
+++ client/src/components/carousel/Carousel.jsx
@@ -16,38 +16,6 @@ export default function Carousel() {
     }
   };
 
-  const prevImage = () => {
-    switch (currentImage) {
-      case "image_2":
-        setCurrentImage("image_1");
-        break;
-      case "image_3":
-        setCurrentImage("image_2");
-        break;
-      case "image_1":
-        setCurrentImage("image_3");
-        break;
-      default:
-        break;
-    }
-  };
-
-  const nextImage = () => {
-    switch (currentImage) {
-      case "image_1":
-        setCurrentImage("image_2");
-        break;
-      case "image_2":
-        setCurrentImage("image_3");
-        break;
-      case "image_3":
-        setCurrentImage("image_1");
-        break;
-      default:
-        break;
-    }
-  };
-
   useEffect(() => {
     getPics();
     console.log("aqui", productPics[currentImage]);
@@ -58,22 +26,7 @@ export default function Carousel() {
         <h1>Welcome to NFT Store</h1>
         <h3>We're here to help you buy digital assets without stress</h3>
       </div>
-      <div>
-        <div className="carousel-container">
-          <img
-            src={productPics[currentImage]}
-            alt={`Image ${currentImage.split(" ")[1]}`}
-            className="product-image"
-          />
-        </div>
-
-        <button className="prev-button" onClick={prevImage}>
-          Previous
-        </button>
-        <button className="next-button" onClick={nextImage}>
-          Next
-        </button>
-      </div>
+      <div></div>
     </div>
   );
 }
