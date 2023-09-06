61606e36ca27b6a81c748472821335d1f1905adc
Andrea <anfrescortes@gmail.com>
2023-09-06 21:20:34 +0200

    ordardata ???

--- client/src/components/orders/Orders.jsx
+++ client/src/components/orders/Orders.jsx
@@ -1,89 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
+function Orders() {
+  const [orders, setOrders] = useState([]);
+  const [user_id, setUser_id] = useState("");
 
+  console.log(user_id);
 
+  const handleClick = () => {
+    if (routeUserId) {
+      getOrders(routeUserId);
+    }
+  };
 
+  // //orderHIstory
+  useEffect(() => {
+    getOrders();
+  }, [user_id]);
 
-function Orders () {
+  const getUserId = () => {
+    const userId = localStorage.getItem("user");
+    setUser_id(userId);
+    console.log(userId);
+  };
 
-     
+  //Group orders
+  const groupOrdersByOrderNumber = (orderData) => {
+    const groupedOrders = {};
+    orderData.forEach((order) => {
+      if (!groupedOrders[order.id]) {
+        groupedOrders[order.id] = [];
+      }
+      groupedOrders[order.id].push(order);
+    });
+    return groupedOrders;
+  };
 
-        const [orders, setOrders] = useState([]);
-        const [user_id, setUser_id] = useState("");
-     
+  //fetch order history based on user_id
+  const getOrders = async () => {
+    getUserId();
+    let options = {
+      method: "GET",
+    };
+    try {
+      if (user_id) {
         console.log(user_id);
-        
-      
-        const handleClick = () => {
-          if (routeUserId) {
-            getOrders(routeUserId);
-          }
-        };
+        const response = await fetch(`/api/Orders/all/${user_id}`, options);
+        const data = await response.json();
 
-        // //orderHIstory
-        useEffect (() => {
-         
-          getOrders();
-        },[user_id]);
+        // Group orders by order number
+        const groupedOrders = groupOrdersByOrderNumber(data);
 
+        setOrders(groupedOrders);
+      }
+      //group
+    } catch (err) {
+      console.log(`network error: ${err.message}`);
+    }
+  };
 
-        const getUserId=() => {
-          const userId = localStorage.getItem("user")
-          setUser_id(userId)
-          console.log(userId)
-        }
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <h3>Orders</h3>
 
-
-        //Group orders
-        const groupOrdersByOrderNumber = (orderData) => {
-          const groupedOrders = {};
-          orderData.forEach((order) => {
-            if (!groupedOrders[order.id]) {
-              groupedOrders[order.id] = [];
-            }
-            groupedOrders[order.id].push(order);
-          });
-          return groupedOrders;
-        };
-
-
-
-            //fetch order history based on user_id
-        const getOrders = async () => {
-           getUserId()
-           let options = {
-             method: "GET",
-        };
-        try {
-          if (user_id) {
-            console.log(user_id)
-            const response = await fetch (`/api/Orders/all/${user_id}`, options);
-            const data = await response.json();
-    
-          // Group orders by order number
-           const groupedOrders = groupOrdersByOrderNumber(data);
-
-          setOrders(groupedOrders);
-
-          }
-          //group
-      
-      
-        } catch (err) {
-          console.log(`network error: ${err.message}`);
-        }
-        };
-
-
-
-
-
-return (
-    <div style={{marginTop: "100px"}}>
-        <h3>Orders</h3>
-       
-        
-       <div>
+      <div>
         {Object.keys(orders).map((orderNumber) => (
           <div key={orderNumber}>
             <h4>Order Number: {orderNumber}</h4>
@@ -106,17 +85,8 @@ return (
           </div>
         ))}
       </div>
-      
-      
-      
-</div>  
-
-    
-
-
-)
-
-
+    </div>
+  );
 }
 
 export default Orders;
