6fa2f241943faaa37dd9b51a07e03e7708e975eb
Paula Burghelea <paulaburghelea.github@gmail.com>
2024-01-23 09:19:17 +0000

    mobile menu styling

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -7,9 +7,9 @@ export default function Menu() {
 				<span className="sr-only btn__info">Open Menu</span>
 				<span className="sr-only btn__info">Close Menu</span>
 				<span className="btn__icon icon">
-				<span className=""></span>
-				<span></span>
-				<span></span>
+				<span className="w-2 h-1 bg-primary-800 line mr-2 p-0 rounded-full"></span>
+				<span className="w-9 h-1 bg-primary-800 line m-0 p-0 rounded-full"></span>
+				<span className="w-2 h-1 bg-primary-800 line ml-2 p-0 rounded-full"></span>
 				</span>
 			</button>
 		</div>
