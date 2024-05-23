36f032efa6c28e223e7e2c6bc7f55a28e058011f
Paula Burghelea <paulaburghelea.github@gmail.com>
2024-05-23 16:22:20 +0100

    whatever I did

--- routes/auth.js
+++ routes/auth.js
@@ -12,19 +12,18 @@ const selectByKey = require("./utils/selectByKey");
 const supersecret = process.env.SUPER_SECRET;
 
 router.post("/register", async (req, res) => {
-  const { firstname, lastname, email, password, marketing } = req.body;
+	const { firstname, lastname, email, password, marketing } = req.body;
 
 	try {
-   const hash = await bcrypt.hash(password, saltRounds);
+		const hash = await bcrypt.hash(password, saltRounds);
 
-   await db(
-      `INSERT INTO users (firstname, lastname, email, password, marketing) VALUES 
-		('${firstname}', '${lastname}', '${email}', '${hash}', ${marketing})`
-   );
-
-   res.send({ message: "Register successful" });
+		await db(
+			`INSERT INTO users (firstname, lastname, email, password, marketing) VALUES 
+				('${firstname}', '${lastname}', '${email}', '${hash}', ${marketing})`
+		);
+		res.send({ message: "Register successful" });
 	} catch (err) {
-   res.status(400).send({ message: err.message });
+		res.status(400).send({ message: err.message });
 	}
 });
 
@@ -46,33 +45,38 @@ router.post("/login", async (req, res) => {
 	const { email, password } = req.body;
 
 	try {
-   const results = await db(`SELECT * FROM users WHERE email = "${email}";`);
+	const results = await db(`SELECT * FROM users WHERE email = "${email}";`);
 	const user = results.data[0];
 	// const data = selectByKey("users", "email", email);
 	// const user = data[0];
-   if (user) {
-      const user_id = user.id;
+	if (user) {
+    	const user_id = user.id;
 
-      const correctPassword = await bcrypt.compare(password, user.password);
+    	const correctPassword = await bcrypt.compare(password, user.password);
 
-      if (!correctPassword) throw new Error("Incorrect password");
+    	if (!correctPassword) throw new Error("Incorrect password");
 
-      const token = jwt.sign({ user_id }, supersecret);
-      res.send({ message: "Login successful, here is your token", token, user:user});
-   } else {
-      throw new Error("User does not exist");
-   }
+    	const token = jwt.sign({ user_id }, supersecret);
+    	res.send({ message: "Login successful, here is your token", token, user:user});
+	} else {
+    	throw new Error("User does not exist");
+	}
 	} catch (err) {
-   res.status(400).send({ message: err.message });
+	res.status(400).send({ message: err.message });
 	}
 });
 
 router.get("/profile", userShouldBeLoggedIn, async function (req, res, next) {
 	//console.log(req.user_id);
 	//const data = selectByKey("users", "id", req.user_id);
-	const result = await db(`SELECT * FROM users WHERE id = ${req.user_id};`)
-	console.log(result);
-	res.send(result.data[0]);
+	try {
+		const result = await db(`SELECT * FROM users WHERE id = ${req.user_id};`)
+		console.log(result);
+		res.send(result.data[0]);
+	} catch (err) {
+		res.status(400).send({ message: err.message });
+	}
+
 });
 
 module.exports = router;
